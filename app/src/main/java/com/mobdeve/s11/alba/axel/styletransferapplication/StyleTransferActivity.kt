5260300f2c6f2dfb563923866a6ca584325a4a58
Axel Alba <albaaxel@yahoo.com>
2021-08-24 11:13:32 +0800

    Added astyle transfer activity

---
+++ app/src/main/java/com/mobdeve/s11/alba/axel/styletransferapplication/StyleTransferActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s11.alba.axel.styletransferapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StyleTransferActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_style_transfer)
+    }
+}

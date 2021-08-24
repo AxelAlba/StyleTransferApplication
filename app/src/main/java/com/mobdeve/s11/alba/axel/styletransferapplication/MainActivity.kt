2fc6191180677d8da708613ad7afacaefe4e4cb6
Axel Alba <albaaxel@yahoo.com>
2021-08-24 11:28:13 +0800

    Activities are now connected

--- app/src/main/java/com/mobdeve/s11/alba/axel/styletransferapplication/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/alba/axel/styletransferapplication/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.mobdeve.s11.alba.axel.styletransferapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStyleTransfer = findViewById<Button>(R.id.btn_style_transfer)
+            btnStyleTransfer.setOnClickListener{
+                val intent = Intent(this, StyleTransferActivity::class.java)
+                startActivity(intent)
+            }
     }
 }

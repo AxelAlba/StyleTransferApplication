cd2d47f98141b45fe2fcc486d9e390723af3b4e9
Axel Alba <albaaxel@yahoo.com>
2021-09-12 13:49:37 +0800

    gallery module implementation

--- app/src/main/java/com/mobdeve/s11/alba/axel/styletransferapplication/MainActivity.java
+++ app/src/main/java/com/mobdeve/s11/alba/axel/styletransferapplication/MainActivity.java
@@ -1,12 +1,27 @@
 package com.mobdeve.s11.alba.axel.styletransferapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.os.Bundle;
 import android.view.View;
@@ -16,8 +31,14 @@ import android.widget.ImageButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
+import android.util.Log;
+import android.widget.ImageView;
 
+import com.mobdeve.s11.alba.axel.styletransferapplication.ImageFilePath;
 
 //private String TAG = "MainActivity";
 
@@ -29,7 +50,9 @@ public class MainActivity extends AppCompatActivity {
     private boolean isFavorite;
     private FloatingActionButton btnStyleTransfer;
     private FirestoreHelper db_helper;
-
+    private int SELECT_PICTURE = 200;
+    private ImageView imageView;
+    private String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,14 +69,81 @@ public class MainActivity extends AppCompatActivity {
         // initialize recycler view
         this.initRecyclerView();
 
+        imageView = (ImageView) findViewById(R.id.my_avatar);
+
         // FAB action
         btnStyleTransfer = findViewById(R.id.btn_style_transfer);
         btnStyleTransfer.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+//                startActivity(intent);
+                selectImage(MainActivity.this);
             }
         });
+    }
+    private void selectImage(Context context) {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Import an image");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (options[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                    startActivity(intent);
+
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+
+                    imageView.setImageURI(selectedImageUri);
+
+                    BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+                    Bitmap image = drawable.getBitmap();
+
+                    String filename= "";
+                    try {
+                        filename = Long.toHexString(Double.doubleToLongBits(Math.random())) + ".jpg";
+                        FileOutputStream fileOutputStream = this.openFileOutput(filename, Context.MODE_PRIVATE);
+                        boolean status = image.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
+                        Log.d(TAG, "saved successfully " + getFilesDir().toPath());
+                        fileOutputStream.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Log.d(TAG, "This is the file path from gallery: " + "" + getFilesDir().toPath() + "/" + filename);
+
+                    Intent intent = new Intent(MainActivity.this, CreatePostActivity.class);
+                    intent.putExtra("IMAGE_URL", "" + getFilesDir().toPath() + "/" + filename);
+                    startActivity(intent);
+                }
+            }
+        }
     }
 
     private void initRecyclerView() {
